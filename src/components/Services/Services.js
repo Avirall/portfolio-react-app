0626749ce7048300345123fd9c788b6d9ce8b7e8
Aviral Singh <aviral.singh.9794@gmail.com>
2023-07-24 01:37:27 +0530

    react app

--- src/components/Services/Services.js
+++ src/components/Services/Services.js
@@ -10,51 +10,36 @@ const Services = () => {
             My Awesome <span>Services</span>
           </h1>
           <p>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nemo velit
-            ratione quidem minima necessitatibus. Inventore sunt cupiditate
-            pariatur ipsum a laudantium, dolor minus, illo laboriosam
-            consequatur provident cum velit tempore! Lorem ipsum dolor sit amet
-            consectetur adipisicing elit. Fuga placeat rem illo molestias
-            mollitia voluptatem dicta voluptate eius, laboriosam quaerat officia
-            sunt harum accusantium rerum voluptas cumque incidunt commodi eaque?
+          I have extensive experience in developing web applications using the MERN stack (MongoDB, Express, React and Node.js), as well as performing data analysis using Python and various libraries. I can create responsive and user-friendly front-end interfaces, as well as robust and scalable back-end solutions. I am proficient in HTML, CSS, JavaScript, Bootstrap, and other web technologies. I am also skilled in using tools such as Git, GitHub, VS Code. I am passionate about learning new skills and technologies to enhance my portfolio and deliver high-quality services to my clients.
           </p>
           <button>Hire Me</button>
         </div>
         {/* item */}
         <div className="service-item-container">
           <div className="services-item">
-            <i className="fa-solid fa-code"></i>
+            <i className="fa-solid fa-tablet-alt"></i>
             <div className="item-desc">
-              <h3>Web Development</h3>
+              <h3>MERN Stack</h3>
               <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod
-                laboriosam, rerum cupiditate sint praesentium nam architecto
-                ullam in, minima reiciendis provident amet doloremque facilis
-                obcaecati neque nemo totam. Tempore, illo.
+              I have developed several projects using the MERN stack, such as a social media platform, a blog site, and a e-commerce store. I am passionate about learning new technologies and creating user-friendly and responsive web interfaces
               </p>
             </div>
           </div>
           <div className="services-item">
             <i className="fa-solid fa-desktop"></i>
             <div className="item-desc">
-              <h3>Desktop Development</h3>
+              <h3>Front-End Development</h3>
               <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod
-                laboriosam, rerum cupiditate sint praesentium nam architecto
-                ullam in, minima reiciendis provident amet doloremque facilis
-                obcaecati neque nemo totam. Tempore, illo.
+              I have a strong knowledge of HTML, CSS, JavaScript, and various frameworks and libraries such as React, Bootstrap, and jQuery. I am proficient in using web development tools such as Git, VS Code, and Chrome DevTools.
               </p>
             </div>
           </div>
           <div className="services-item">
-            <i className="fa-solid fa-tablet-alt"></i>
+            <i className="fa-solid fa-code"></i>
             <div className="item-desc">
-              <h3>U/X Design</h3>
+              <h3>Data Analysis</h3>
               <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod
-                laboriosam, rerum cupiditate sint praesentium nam architecto
-                ullam in, minima reiciendis provident amet doloremque facilis
-                obcaecati neque nemo totam. Tempore, illo.
+              I have strong skills in Python, SQL, R, and various data visualization tools. I have successfully delivered data-driven solutions for clients in various domains, such as e-commerce, healthcare, and education.
               </p>
             </div>
           </div>
